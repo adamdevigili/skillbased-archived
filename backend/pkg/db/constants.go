ec27dbee7419c699b4274a1e76326436ecc97774
Adam Devigili <adam.devigili@gmail.com>
2020-04-28 11:53:38 -0500

    update gitignore

--- backend/pkg/db/constants.go
+++ backend/pkg/db/constants.go
@@ -4,6 +4,7 @@ import (
 	"github.com/adamdevigili/balancer.team/pkg/models"
 )
 
+// Temporary in-memory data store
 var (
 	TeamsMem   = map[string]*models.Team{}
 	SportsMem  = map[string]*models.Sport{"ultimate-id": &models.UltimateFrisbee}
