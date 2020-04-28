e6e11225d55a6a0b6b8e5c7d1ecb7a6809de3783
Adam Devigili <adam.devigili@gmail.com>
2020-04-28 14:27:37 -0500

    update README badge to buddy.works

--- backend/pkg/handlers/teams_list.go
+++ backend/pkg/handlers/teams_list.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/adamdevigili/balancer.team/pkg/db"
+	"github.com/adamdevigili/balancer.team/pkg/models"
+	"github.com/labstack/echo"
+)
+
+func (h *Handler) ListTeams(c echo.Context) error {
+	teamList := make([]models.Team, len(db.SportsMem))
+	for _, t := range db.TeamsMem {
+		teamList = append(teamList, *t)
+	}
+
+	return c.JSON(http.StatusOK, teamList)
+}
