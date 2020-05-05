d33296003c5a7320390b579d66054ee40e073813
Adam Devigili <adam.devigili@gmail.com>
2020-05-04 21:42:40 -0500

    update waiting message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,46 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ping: 'Fuck you Siva'
+		};
+	}
 
-class App extends React.Component{
+	componentDidMount() {
+		axios.get('/api/ping').then((res, err) => {
+			if (res.status === 200) {
+				this.setState({
+					ping: res.data.message
+				});
+			} else {
+				console.log(err);
+			}
+		});
+	}
 
-    constructor(props){
-        super(props);
-        this.state = {
-            ping: "Waiting"
-        }
-    }
-
-    componentDidMount() {
-        axios.get('/api/ping')
-            .then((res, err) => {
-                if(res.status === 200) {
-                    this.setState({
-                        ping: res.data.message
-                    })
-                }
-                else {
-                    console.log(err);
-                }
-            })
-    }
-
-    render(){
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <a
-                        className="App-link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {this.state.ping}
-                    </a>
-                </header>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<a className="App-link" target="_blank" rel="noopener noreferrer">
+						{this.state.ping}
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
