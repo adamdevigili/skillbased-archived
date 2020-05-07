ac0ff151b969f3c7d979a032f74d71ece91ec23a
Adam Devigili <adam.devigili@gmail.com>
2020-05-07 13:17:41 -0500

    add static file serving to API

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/adamdevigili/skillbased.io/pkg/db"
 	"github.com/adamdevigili/skillbased.io/pkg/server"
-
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,9 +10,12 @@ func main() {
 	dbConn := db.InitDB()
 
 	e := echo.New()
-
 	server.InitRoutes(e, dbConn)
 
+	// Serve static files for non-operational API calls (browser visits to api subdomain
+	e.Static("/", "./static")
+
 	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":8081"))
+
 }
