a14b47c122354a820a757d1fea216eac7941b04d
Adam Devigili <adam.devigili@gmail.com>
2020-06-05 12:45:52 -0500

    add fe routing to api subdomain

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import APIApp from './APIApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let host = window.location.host
+console.log(host)
+const parsedData = host.split(".");
+console.log("hey")
+console.log(parsedData)
+
+if (parsedData[0] == "api") {
+    const subDomain = parsedData[0];
+    ReactDOM.render(
+        <React.StrictMode>
+            <APIApp />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+} else {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
